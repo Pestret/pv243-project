504f73262c8414dd5cee54f144f6409dea5ee695
Siliarus <matej.novotny2@gmail.com>
2013-04-17 17:48:35 +0200

    dao updated

--- project/explosivesShop/src/main/java/cz/muni/fi/pv243/data/UserManagerImpl.java
+++ project/explosivesShop/src/main/java/cz/muni/fi/pv243/data/UserManagerImpl.java
@@ -20,30 +20,31 @@ public class UserManagerImpl implements UserManager {
 	
 	@Override
 	public void create(User user) {
-		
-
+		if (user.getId() != null) {
+			throw new IllegalArgumentException("je to zosrate");
+		}
+		//TODO validation everywhere
+		em.persist(user);
 	}
 
 	@Override
 	public void update(User user) {
-		// TODO Auto-generated method stub
-
+		if (user.getId() == null) {
+			throw new IllegalArgumentException("je to zosrate");
+		}
+		em.merge(user);
 	}
 
 	@Override
 	public void delete(User user) {
-		// TODO Auto-generated method stub
-
+		if (user.getId() == null) {
+			throw new IllegalArgumentException("je to zosrate");
+		}
+		em.remove(user);
 	}
 
 	@Override
 	public List<User> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<User> findByName(String name) {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<User> criteria = cb.createQuery(User.class);
         Root<User> us = criteria.from(User.class);
@@ -55,7 +56,25 @@ public class UserManagerImpl implements UserManager {
 	}
 
 	@Override
+	public List<User> findByName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("je to zosrate");
+		}
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<User> criteria = cb.createQuery(User.class);
+        Root<User> us = criteria.from(User.class);
+        // Swap criteria statements if you would like to try out type-safe criteria queries, a new
+        // feature in JPA 2.0
+        // criteria.select(member).orderBy(cb.asc(member.get(Member_.name)));
+        criteria.select(us).where(cb.equal(us.get("name"), name));
+        return em.createQuery(criteria).getResultList();
+	}
+
+	@Override
 	public User findByEmail(String email) {
+		if (email == null) {
+			throw new IllegalArgumentException("je to zosrate");
+		}
 		CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<User> criteria = cb.createQuery(User.class);
         Root<User> us = criteria.from(User.class);
