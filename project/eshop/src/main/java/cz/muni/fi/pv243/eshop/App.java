b04e6a8ce08afd3bd45ddd8fae834d98e80ec55d
Siliarus <matej.novotny2@gmail.com>
2013-04-17 15:43:37 +0200

    projectCreated
    Created new project

---
+++ project/eshop/src/main/java/cz/muni/fi/pv243/eshop/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv243.eshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
