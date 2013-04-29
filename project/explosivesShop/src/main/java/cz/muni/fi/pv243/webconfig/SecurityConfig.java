ed123f2eb328878fdfda18da9fa927287000acd0
Jaromír Benda <JaromirBenda@gmail.com>
2013-04-29 15:10:28 +0200

    little change

--- project/explosivesShop/src/main/java/cz/muni/fi/pv243/webconfig/SecurityConfig.java
+++ project/explosivesShop/src/main/java/cz/muni/fi/pv243/webconfig/SecurityConfig.java
@@ -18,7 +18,7 @@ public interface SecurityConfig {
 
 		@ViewPattern("/tables.xhtml")
 		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
+		@AccessDeniedView("/login.xhtml")
 		@LoggedIn
 		@Admin
 		@RestrictAtPhase({ PhaseIdType.RESTORE_VIEW,
@@ -27,62 +27,19 @@ public interface SecurityConfig {
 
 		@ViewPattern("/admin_page.xhtml")
 		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
+		@AccessDeniedView("/login.xhtml")
 		@LoggedIn
 		@Admin
 		@RestrictAtPhase({ PhaseIdType.RESTORE_VIEW,
 				PhaseIdType.INVOKE_APPLICATION })
 		ADMIN_PAGE,
 
-		@ViewPattern("/seller_page.xhtml")
-		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
-		@LoggedIn
-//		@Seller
-		@RestrictAtPhase({ PhaseIdType.RESTORE_VIEW,
-				PhaseIdType.INVOKE_APPLICATION })
-		SELLER_PAGE,
-
 		@ViewPattern("/orders.xhtml")
 		@LoginView("/login.xhtml")
-		@AccessDeniedView("/denied.xhtml")
+		@AccessDeniedView("/login.xhtml")
 		@LoggedIn
 		@RestrictAtPhase({ PhaseIdType.RESTORE_VIEW,
 				PhaseIdType.INVOKE_APPLICATION })
 		TEST,
-
-		/*
-		 * @ViewPattern("/view.xhtml")
-		 * 
-		 * @UrlMapping(pattern="/view/#{id}/") VIEW,
-		 * 
-		 * @ViewPattern("/edit.xhtml")
-		 * 
-		 * @LoginView("/login.xhtml")
-		 * 
-		 * @AccessDeniedView("/denied.xhtml")
-		 * 
-		 * @LoggedIn
-		 * 
-		 * @IsManagerOf
-		 * 
-		 * @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
-		 * PhaseIdType.INVOKE_APPLICATION}) EDIT,
-		 * 
-		 * @ViewPattern("/bakerylist.xhtml") BAKERYLIST,
-		 * 
-		 * @ViewPattern("/managers.xhtml")
-		 * 
-		 * @LoginView("/login.xhtml")
-		 * 
-		 * @AccessDeniedView("/denied.xhtml")
-		 * 
-		 * @LoggedIn
-		 * 
-		 * @Admin
-		 * 
-		 * @RestrictAtPhase({PhaseIdType.RESTORE_VIEW,
-		 * PhaseIdType.INVOKE_APPLICATION}) MANAGERS
-		 */
 	}
 }
