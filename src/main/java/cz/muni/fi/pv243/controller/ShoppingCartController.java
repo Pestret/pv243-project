ad856ba4362ff8ae579fb12a8d5cfc09e22df352
Jaromír Benda <JaromirBenda@gmail.com>
2013-05-03 11:07:22 +0200

    Updated ShoppingCartController

--- src/main/java/cz/muni/fi/pv243/controller/ShoppingCartController.java
+++ src/main/java/cz/muni/fi/pv243/controller/ShoppingCartController.java
@@ -1,16 +1,21 @@
 package cz.muni.fi.pv243.controller;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import org.jboss.seam.security.Identity;
 
+import cz.muni.fi.pv243.model.Product;
 import cz.muni.fi.pv243.model.ShoppingCart;
 import cz.muni.fi.pv243.model.OrderItem;
+import cz.muni.fi.pv243.service.OrderItemManager;
+import cz.muni.fi.pv243.service.ProductManager;
 import cz.muni.fi.pv243.service.ShoppingCartManager;
 
 @Model
@@ -26,10 +31,28 @@ public class ShoppingCartController {
 	 @Inject
 	 private ShoppingCartManager cartManager;
 	 
-	 public ShoppingCartController(){
+	 @Inject 
+	 private ProductManager productManager;
+	 
+	 @Inject
+	 private OrderItemManager orderManager;
+	 
+	 @PostConstruct
+	 public void initShoppingCart(){
+		 List<OrderItem> list = new ArrayList<OrderItem>();
+		 cart.setItems(list);
+		 //cart.setUser(identity.getUser());
+		 cartManager.create(cart);
 	 }
 	 
 	 public void addToCart(Long productId, int quantity){
+		 Product product = productManager.get(productId);
+		 OrderItem order = new OrderItem();
+		 order.setProduct(product);
+		 order.setQuantity(quantity);
+		 orderManager.create(order);
+		 
+		 
 		 
 	 }
 	 
@@ -47,5 +70,23 @@ public class ShoppingCartController {
 	 
 	 public BigDecimal getItemPrice(Long productId){
 		 return new BigDecimal(0);
+	 }
+	 
+	 public void clearCart(){
+		 
+	 }
+	 
+	 private void addOrderItem(OrderItem item){
+		 List<OrderItem> items = cart.getItems();
+		 if(items.contains(item)){
+			for(int i=0;i<items.size();i++){
+				OrderItem inCart = items.get(i);
+				if(inCart.equals(item)){
+					inCart.setQuantity((inCart.getQuantity()+item.getQuantity()));
+				}
+			}
+		 } else {
+			 items.add(item);
+		 }
 	 }
 }
