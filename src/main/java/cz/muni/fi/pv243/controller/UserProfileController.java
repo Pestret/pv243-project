06db77fee79d193e717e407439c895824168f557
Jaromír Benda <JaromirBenda@gmail.com>
2013-05-09 17:21:20 +0200

    User profile

--- src/main/java/cz/muni/fi/pv243/controller/UserProfileController.java
+++ src/main/java/cz/muni/fi/pv243/controller/UserProfileController.java
@@ -1,5 +1,51 @@
 package cz.muni.fi.pv243.controller;
 
-public class UserProfileController {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
 
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import org.jboss.seam.security.Identity;
+
+import cz.muni.fi.pv243.model.OrderItem;
+import cz.muni.fi.pv243.model.ShoppingCart;
+import cz.muni.fi.pv243.service.OrderItemManager;
+import cz.muni.fi.pv243.service.ProductManager;
+import cz.muni.fi.pv243.service.ShoppingCartManager;
+import cz.muni.fi.pv243.service.UserManager;
+
+@SessionScoped
+@Model
+public class UserProfileController implements Serializable {
+
+	private static final long serialVersionUID = 2118963139482L;
+	
+	@Inject
+	private FacesContext facesContext;
+
+	@Inject
+	private Identity identity;
+
+	@Inject
+	private ShoppingCartManager cartManager;
+
+	@Inject
+	private ProductManager productManager;
+
+	@Inject
+	private OrderItemManager orderManager;
+
+	public BigDecimal priceOfOrder(Long id) {
+		BigDecimal num = new BigDecimal(0);
+		ShoppingCart cart = cartManager.get(id);
+		List<OrderItem> items = cart.getItems();
+		for(OrderItem item : items){
+			num = num.add(item.getProduct().getPrice().multiply(new BigDecimal(item.getQuantity())));
+		}
+		return num;
+	}
 }
