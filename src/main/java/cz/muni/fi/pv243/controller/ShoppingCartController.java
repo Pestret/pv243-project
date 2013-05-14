df3dc34548dd298b2d764f75552aa2b2b544fa57
Jan Richter <j2.richt@gmail.com>
2013-05-14 13:52:41 +0200

    Order confirmation page

--- src/main/java/cz/muni/fi/pv243/controller/ShoppingCartController.java
+++ src/main/java/cz/muni/fi/pv243/controller/ShoppingCartController.java
@@ -18,105 +18,127 @@ import cz.muni.fi.pv243.service.OrderItemManager;
 import cz.muni.fi.pv243.service.ProductManager;
 import cz.muni.fi.pv243.service.ShoppingCartManager;
 import cz.muni.fi.pv243.service.UserManager;
+
+import java.io.IOException;
 import java.io.Serializable;
 
 @SessionScoped
 @Model
 public class ShoppingCartController implements Serializable {
 
-	 /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2118963139420656282L;
 
 	@Inject
-	 private FacesContext facesContext;
+	private FacesContext facesContext;
 
-	 @Inject
-	 private Identity identity;
-	 
-	 @Inject
-	 private ShoppingCart cart;
-	 
-	 @Inject
-	 private ShoppingCartManager cartManager;
-	 
-	 @Inject 
-	 private ProductManager productManager;
-	 
-	 @Inject
-	 private OrderItemManager orderManager;
-	 
-	 @Inject
-	 private UserManager userManager;
-	 
-	 private int quantity = 1;
-	 private BigDecimal totalPrice = new BigDecimal(0);
-	 
-	 public int getQuantity() {
+	@Inject
+	private Identity identity;
+
+	@Inject
+	private ShoppingCart cart;
+
+	@Inject
+	private ShoppingCartManager cartManager;
+
+	@Inject
+	private ProductManager productManager;
+
+	@Inject
+	private OrderItemManager orderManager;
+
+	@Inject
+	private UserManager userManager;
+
+	private int quantity = 1;
+	private BigDecimal totalPrice = new BigDecimal(0);
+
+	public int getQuantity() {
 		return quantity;
 	}
-	 
-	 public void setQuantity(int quantity){
-		 this.quantity = quantity;
-	 }
 
-	public void addToCart (Long productId, Long quantity) {
-		 if(quantity == null){
-			 throw new IllegalArgumentException("Long number quantity was null.");
-		 }
-		 addToCart(productId, quantity.intValue());
-	 }
-	 
-	 public void addToCart(Long productId, int quantity){
-		 Product product = productManager.get(productId);
-		 List<OrderItem> items = cart.getItems();
-		 for (OrderItem element : items) {
-			 if(element.getProduct().equals(product)){
-				 element.setQuantity(element.getQuantity()+quantity);
-				 setQuantity(1);
-				 return;
-			 }
-		 }
-		 OrderItem order = new OrderItem();
-		 order.setProduct(product);
-		 order.setQuantity(quantity);
-		 items.add(order);
-		 setQuantity(1);
-	 }
-	 
-	 public void deleteOrderItemInCart(Product product){
-		 List<OrderItem> items = cart.getItems();
-		 for(OrderItem forItem : items){
-			 if(forItem.getProduct().equals(product)){
-				 items.remove(forItem);
-			 }
-		 }
-	 }
-	 
-	 public BigDecimal getTotalPrice(){
-		 totalPrice = new BigDecimal(0);
-		 for(OrderItem item: cart.getItems()){
-			 totalPrice = totalPrice.add(item.getProduct().getPrice().multiply(new BigDecimal(item.getQuantity())));
-		 }
-		 return totalPrice;
-	 }
-	 
-	 public List<OrderItem> getAll(){
-		 return cart.getItems();
-	 }
-	 
-	 public void clearCart(){
-		 cart = new ShoppingCart();
-		 cart.setFinished(false);
-		 cart.setItems(new ArrayList<OrderItem>());
-	 }
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
-	 public void order(){
-		 //TODO
-		 //check if user is logged in, otherwise next line will be nullpointer
-		 cart.setUser(userManager.findByEmail(identity.getUser().getId()));
-		 cartManager.create(cart);
-		 clearCart();
-	 }
+	public void addToCart(Long productId, Long quantity) {
+		if (quantity == null) {
+			throw new IllegalArgumentException("Long number quantity was null.");
+		}
+		addToCart(productId, quantity.intValue());
+	}
+
+	public void addToCart(Long productId, int quantity) {
+		Product product = productManager.get(productId);
+		List<OrderItem> items = cart.getItems();
+		for (OrderItem element : items) {
+			if (element.getProduct().equals(product)) {
+				element.setQuantity(element.getQuantity() + quantity);
+				setQuantity(1);
+				return;
+			}
+		}
+		OrderItem order = new OrderItem();
+		order.setProduct(product);
+		order.setQuantity(quantity);
+		items.add(order);
+		setQuantity(1);
+	}
+
+	public void deleteOrderItemInCart(Product product) {
+		List<OrderItem> items = cart.getItems();
+		for (OrderItem forItem : items) {
+			if (forItem.getProduct().equals(product)) {
+				items.remove(forItem);
+			}
+		}
+	}
+
+	public BigDecimal getTotalPrice() {
+		totalPrice = new BigDecimal(0);
+		for (OrderItem item : cart.getItems()) {
+			totalPrice = totalPrice.add(item.getProduct().getPrice()
+					.multiply(new BigDecimal(item.getQuantity())));
+		}
+		return totalPrice;
+	}
+
+	public List<OrderItem> getAll() {
+		return cart.getItems();
+	}
+
+	public void clearCart() {
+		cart = new ShoppingCart();
+		cart.setFinished(false);
+		cart.setItems(new ArrayList<OrderItem>());
+	}
+
+	public void order() {
+		// TODO
+		// check if user is logged in, otherwise next line will be nullpointer
+		cart.setUser(userManager.findByEmail(identity.getUser().getId()));
+
+		try {
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("order.jsf");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void confirm() {
+		cartManager.create(cart);
+		clearCart();
+
+		try {
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("index.jsf");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
