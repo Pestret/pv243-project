13e763208d036f8aa81593b63c0862e6f024eea3
Petr Suchy <suchar.15@seznam.cz>
2013-05-22 16:21:30 +0200

    Arqullian tests - UserTest updated

--- src/test/java/cz/muni/fi/pv243/test/OrderItemManagerTest.java
+++ src/test/java/cz/muni/fi/pv243/test/OrderItemManagerTest.java
@@ -1,30 +1,13 @@
 package cz.muni.fi.pv243.test;
 
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import cz.muni.fi.pv243.model.OrderItem;
-import cz.muni.fi.pv243.model.Product;
-import cz.muni.fi.pv243.model.ShoppingCart;
-import cz.muni.fi.pv243.model.User;
-import cz.muni.fi.pv243.service.OrderItemManager;
-import cz.muni.fi.pv243.service.OrderItemManagerImpl;
-import cz.muni.fi.pv243.service.ProductManager;
-import cz.muni.fi.pv243.service.ProductManagerImpl;
-import cz.muni.fi.pv243.service.ShoppingCartManager;
-import cz.muni.fi.pv243.service.ShoppingCartManagerImpl;
-import cz.muni.fi.pv243.service.UserManager;
-import cz.muni.fi.pv243.service.UserManagerImpl;
-import cz.muni.fi.pv243.util.Resources;
 
 @RunWith(Arquillian.class)
 public class OrderItemManagerTest {
@@ -33,19 +16,19 @@ public class OrderItemManagerTest {
 	public static Archive<?> createTestArchive() {
 		return ShrinkWrap
 				.create(WebArchive.class, "test.war")
-				.addClasses(User.class, ShoppingCart.class, OrderItem.class,
-						Product.class, UserManager.class,
-						UserManagerImpl.class,ProductManagerImpl.class, 
-						ShoppingCartManagerImpl.class, OrderItemManagerImpl.class, ProductManager.class, 
-						ShoppingCartManager.class, OrderItemManager.class, Resources.class)
+				.addPackages(true, "cz.muni.fi.pv243")
+				.addPackage(org.picketlink.idm.api.User.class.getPackage())
+				.addPackage(org.jboss.seam.security.BaseAuthenticator.class.getPackage())
+				.addClasses(org.picketlink.idm.api.User.class)
 				.addAsResource("META-INF/test-persistence.xml",
 						"META-INF/persistence.xml")
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
 				.addAsWebInfResource("test-ds.xml");
 	}
 
-	@Inject
-	Logger log;
-
+	@Test
+	public void test1(){
+		
+	}
 
 }
--- src/test/java/cz/muni/fi/pv243/test/UserManagerTest.java
+++ src/test/java/cz/muni/fi/pv243/test/UserManagerTest.java
@@ -1,8 +1,8 @@
 package cz.muni.fi.pv243.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import javax.ejb.EJBException;
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.security.util.Base64;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -21,6 +22,7 @@ import cz.muni.fi.pv243.model.OrderItem;
 import cz.muni.fi.pv243.model.Product;
 import cz.muni.fi.pv243.model.ShoppingCart;
 import cz.muni.fi.pv243.model.User;
+import cz.muni.fi.pv243.security.Encoder;
 import cz.muni.fi.pv243.service.UserManager;
 import cz.muni.fi.pv243.service.UserManagerImpl;
 import cz.muni.fi.pv243.util.Resources;
@@ -34,7 +36,10 @@ public class UserManagerTest {
 				.create(WebArchive.class, "test.war")
 				.addClasses(User.class, ShoppingCart.class, OrderItem.class,
 						Product.class, UserManager.class,
-						UserManagerImpl.class, Resources.class)
+						UserManagerImpl.class, Resources.class, Encoder.class)
+				.addPackage(org.picketlink.idm.api.User.class.getPackage())
+				.addPackage(Base64.class.getPackage())
+				.addPackage("cz.muni.fi.pv243.model.validation")
 				.addAsResource("META-INF/test-persistence.xml",
 						"META-INF/persistence.xml")
 				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
@@ -50,8 +55,8 @@ public class UserManagerTest {
 	@Test
 	public void findByEmailTest() {
 		User user = new User();
-		user.setName("Pepa z depa");
-		user.setEmail("nebuduTo@milujipraci.cz");
+		user.setName("Pepa Depa");
+		user.setEmail("nebuduTo6@milujipraci.cz");
 		user.setAddress("doma");
 		user.setPasswordHash("totalniH4sH");
 		userManager.create(user);
@@ -62,63 +67,67 @@ public class UserManagerTest {
 	public void createUser() {
 		User user = new User();
 		user.setName("Pepa Depa");
-		user.setEmail("nebuduTo@milujipraci.cz");
+		user.setEmail("nebuduTo2@milujipraci.cz");
 		user.setAddress("doma");
 		user.setPasswordHash("totalniH4sH");
 		userManager.create(user);
-		
+
 		User user2 = new User();
-		user2.setName("Pepa z depa");
-		user2.setEmail("nebuduTo@milujipraci.cz");
+		user2.setName("Pepa Depa");
+		user2.setEmail("nebuduTo2@milujipraci.cz");
 		user2.setAddress("doma");
 		user2.setPasswordHash("totalniH4sH");
 		user2.setId(9l);
-		try{
+		try {
 			userManager.create(user2);
-		} catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
-		try{
+		try {
 			userManager.create(null);
-		} catch (IllegalArgumentException e){
-			
+		} catch (EJBException e) {
+
 		}
 	}
-	
+
 	@Test
 	public void createUserName() {
 		User user = new User();
 		user.setName("");
-		user.setEmail("nebuduTo@milujipraci.cz");
+		user.setEmail("nebuduTo3@milujipraci.cz");
 		user.setAddress("doma");
 		user.setPasswordHash("totalniH4sH");
-		
-		try{
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
 
 		user.setName("jmeno");
-		
-		try{
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
-		
+
 		user.setName(null);
-		
-		try{
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
-	
-		user.setName("Pavel Burke");
+
+		user.setName("Pavel Burkes");
 		userManager.create(user);
 	}
-	
+
 	@Test
 	public void createUserEmail() {
 		User user = new User();
@@ -126,63 +135,61 @@ public class UserManagerTest {
 		user.setEmail("");
 		user.setAddress("doma");
 		user.setPasswordHash("totalniH4sH");
-		
-		try{
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
 
 		user.setEmail("email");
-		
-		try{
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
-		}
-		
-		user.setEmail(null);
-		
-		try{
-			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
-		}
-		
-		user.setEmail("e@e");
-		
-		try{
-			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
-		}
-	
-		user.setEmail("e.e");
-		
-		try{
-			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
 
-		user.setEmail("eee9873434333545454545454545eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee@seznam.com");
-		
-		try{
+		user.setEmail(null);
+
+		try {
 			userManager.create(user);
-		}catch (IllegalArgumentException e){
-			
+			fail();
+		} catch (EJBException e) {
+
 		}
-		user.setEmail("borec@zlesa.com");
+
+		user.setEmail("e@e");
+
+		try {
+			userManager.create(user);
+			fail();
+		} catch (EJBException e) {
+
+		}
+
+		user.setEmail("e.e");
+
+		try {
+			userManager.create(user);
+			fail();
+		} catch (EJBException e) {
+
+		}
+
+		user.setEmail("borec@zlesa.cm");
 		userManager.create(user);
 	}
-	
+
 	@Test
 	public void wrongQueries() {
 		assertNull(userManager.findByEmail("tenTuNeni!"));
 	}
-	
+
 	@Test
-	public void testUsers(){
+	public void testUsers() {
 		log.info(userManager.findAll().toString());
 	}
 }
